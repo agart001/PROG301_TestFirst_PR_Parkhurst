7ab21624519d5d3f03d411d7ca1cb6bf88b7bbc4
agart001 <agart.art@gmail.com>
2023-09-30 14:55:25 -0500

    updated Primes.IsPrime()
    added functionality

--- Algorithms/Primes.cs
+++ Algorithms/Primes.cs
@@ -11,16 +11,19 @@ namespace Algorithms
         public bool checkifPrime(int n)
         {
             //we know that 1 is not prime
+            if (n == 1 || n % 2 == 0) return false;
+            if (n == 2) return true;
             
-
             //we now need to find how many factors n has
-            
             //cycle through all the possable factors in n
-            
-
             //if n's factors is greater than 2 it is not prime
+            int max_factor = (int)Math.Sqrt(n);
+            for (int i = 3; i <= max_factor; i += 2)
+            {
+                if (n % i == 0) return false;
+            }
 
-            return false;
+            return true;
         }
 
         public int LargestPrime(int n)
